70a4749c7251d2d27cab556c71b8305d796424e0
Mario Beltrán Alarcón <belco90@gmail.com>
2019-04-29 22:24:48 +0200

    Custom head and base style

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
+import Head from 'next/head';
+
 const Index = () => (
   <div>
+    <Head>
+      <title>Mario Beltrán Alarcón</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto"
+        rel="stylesheet"
+      />
+    </Head>
+
     <p>Coming soon</p>
+
+    <style jsx global>{`
+      body {
+        font-family: 'Roboto', sans-serif;
+        background-color: #1b1f22;
+        color: white;
+      }
+    `}</style>
   </div>
 );
 
