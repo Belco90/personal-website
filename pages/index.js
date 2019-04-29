95af1e4841d8a5cb5ea0d77cffe6caaee2316796
Mario Beltrán Alarcón <belco90@gmail.com>
2019-04-29 23:23:46 +0200

    Add avatar and main box

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,57 @@ const Index = () => (
       />
     </Head>
 
-    <p>Coming soon</p>
+    <div className="root">
+      <div className="wrapper">
+        <img src="/static/avatar.png" alt="avatar" className="avatar" />
+        <div className="main-box">
+          <h1>Mario Beltrán Alarcón</h1>
+        </div>
+      </div>
+    </div>
 
     <style jsx global>{`
       body {
+        margin: 0;
         font-family: 'Roboto', sans-serif;
         background-color: #1b1f22;
         color: white;
+      }
+    `}</style>
+
+    <style jsx>{`
+      .root {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+      }
+
+      .wrapper {
+        padding: 3rem 2rem;
+        display: flex;
+        align-items: center;
+        align-self: center;
+        flex-direction: column;
+        max-width: 100%;
+        text-align: center;
+      }
+
+      .avatar {
+        width: 150px;
+        height: 150px;
+        border-radius: 100%;
+      }
+
+      .main-box {
+        margin-top: 4rem;
+        padding: 0 4rem;
+        border-top: solid 1px white;
+        border-bottom: solid 1px white;
+      }
+
+      .main-box h1 {
+        margin: 4rem 0;
       }
     `}</style>
   </div>
