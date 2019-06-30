efef03b18b2858af4738105609cc688e1c1559c9
Mario Beltrán Alarcón <belco90@gmail.com>
2019-06-30 22:40:04 +0200

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mario Personal Website`,
+    description: `Mario Beltrán Alarcón Personal Website`,
+    author: `Mario Beltrán Alarcón`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,14 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1b1f22`,
+        theme_color: `#1b1f22`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
