487e735039299e7b71a8d2ce9f665d7824cd71bf
Mario Beltrán Alarcón <belco90@gmail.com>
2020-05-26 23:34:05 +0200

    Add coming soon text to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import SEO from '../components/seo';
+import Container from '../components/container';
 
 const Projects = () => {
   return (
     <>
       <SEO title="Projects" />
-      <span>PROJECTS</span>
+      <Container>Coming soon</Container>
     </>
   );
 };
