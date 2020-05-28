59beb5853cc84146147c5a87cd4035e5070b4a9d
Mario Beltrán Alarcón <belco90@gmail.com>
2020-05-28 12:49:45 +0200

    Add first version of projects page

---
+++ src/components/project-card.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Icon,
+  Link,
+  Stack,
+  Tag,
+  Text,
+} from '@chakra-ui/core';
+import PropTypes from 'prop-types';
+import { FaRegStar } from 'react-icons/fa';
+
+const propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  stars: PropTypes.number.isRequired,
+};
+
+const ProjectCard = ({ title, description, language, url, stars, ...rest }) => {
+  return (
+    <Flex direction="column" p={5} shadow="md" borderWidth="1px" {...rest}>
+      <Heading fontSize="xl">
+        <Link href={url} color="primary.500">
+          {title}
+        </Link>
+      </Heading>
+      <Text flex={1} my={4}>
+        {description}
+      </Text>
+      <Stack isInline spacing={8}>
+        <Tag size="md">{language}</Tag>
+        <Flex align="center">
+          <Box as={FaRegStar} aria-label="Stars icon" mr={1} />
+          <Text>{stars}</Text>
+        </Flex>
+      </Stack>
+    </Flex>
+  );
+};
+
+ProjectCard.propTypes = propTypes;
+
+export default ProjectCard;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import SEO from '../components/seo';
 import Container from '../components/container';
+import { SimpleGrid } from '@chakra-ui/core';
+import ProjectCard from '../components/project-card';
+
+const PROJECTS = [
+  {
+    title: 'octoclairvoyant',
+    description:
+      '🔮 Filter and group GitHub repository releases to compare changes with ease ',
+    language: 'TypeScript',
+    url: 'https://github.com/Belco90/octoclairvoyant',
+    stars: 17,
+  },
+  {
+    title: 'eslint-plugin-testing-library',
+    description:
+      'ESLint plugin to follow best practices and anticipate common mistakes when writing tests with Testing Library',
+    language: 'TypeScript',
+    url: 'https://github.com/testing-library/eslint-plugin-testing-library',
+    stars: 176,
+  },
+  {
+    title: 'mastodonte-js',
+    description:
+      '🐘 Un generador de palabras para los mastodontes, cracks y figuras como tú. ',
+    language: 'TypeScript',
+    url: 'https://github.com/Belco90/mastodonte-js',
+    stars: 1,
+  },
+  {
+    title: 'react-advanced-patterns-components',
+    description: 'Iterating through component with advanced React patterns',
+    language: 'JavaScript',
+    url: 'https://github.com/Belco90/react-advanced-patterns-components',
+    stars: 4,
+  },
+];
 
 const Projects = () => {
   return (
     <>
       <SEO title="Projects" />
-      <Container>Coming soon</Container>
+      <Container>
+        <SimpleGrid minChildWidth="300px" spacing={10}>
+          {PROJECTS.map((project) => (
+            <ProjectCard key={project.url} {...project} />
+          ))}
+        </SimpleGrid>
+      </Container>
     </>
   );
 };
