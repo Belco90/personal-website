030a982e4f31ef32942a943b8e8de1db96875000
Mario Beltrán Alarcón <belco90@gmail.com>
2020-06-12 22:04:47 +0200

    First approach for navbar logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Link, Stack } from '@chakra-ui/core';
+import { Box, Link, Stack, Image, Flex } from '@chakra-ui/core';
 import { Link as RouteLink } from 'gatsby';
 import { Location } from '@reach/router'; // gatsby dependency
 
@@ -32,17 +32,28 @@ const Header = (...props) => {
   return (
     <Box as="header" zIndex="banner" {...props}>
       <Container py={5}>
-        <Stack
-          as="nav"
-          isInline
-          spacing={8}
-          align="center"
-          justify="center"
-          shouldWrapChildren
-        >
-          <HeaderLink to="/">Home</HeaderLink>
-          <HeaderLink to="/projects">Projects</HeaderLink>
-        </Stack>
+        <Flex alignItems="center" justifyContent="space-between">
+          <Box width={1 / 3}>
+            <Image src="/logo.svg" height={100} width="auto" />
+          </Box>
+          <Flex
+            width={{ base: '100%', md: 2 / 3 }}
+            justifyContent={{ base: 'end', md: 'initial' }}
+          >
+            <Stack
+              as="nav"
+              width="50%"
+              isInline
+              spacing={8}
+              align="center"
+              justify="center"
+              shouldWrapChildren
+            >
+              <HeaderLink to="/">Home</HeaderLink>
+              <HeaderLink to="/projects">Projects</HeaderLink>
+            </Stack>
+          </Flex>
+        </Flex>
       </Container>
     </Box>
   );
