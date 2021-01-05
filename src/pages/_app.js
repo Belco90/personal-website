fedb457882a8f5a5a79c3d8e88bd818761c3c0ab
Mario Beltrán Alarcón <belco90@gmail.com>
2021-01-05 21:43:25 +0100

    Set gtag up

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,25 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import { Global } from '@emotion/core';
 import customTheme from '../theme';
 import globalStyles from '../globalStyles';
+import * as gtag from '../lib/gtag';
 import 'typeface-open-sans';
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider theme={customTheme}>
       <CSSReset />
