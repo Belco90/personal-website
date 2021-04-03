3b5869d205dca6cf1c6fdf0de95547454b4d4a67
Mario Beltrán Alarcón <belco90@gmail.com>
2021-04-03 19:33:41 +0200

    remove preact usage

--- next.config.js
+++ next.config.js
@@ -1,14 +1,1 @@
-module.exports = {
-  webpack(config, { dev, isServer }) {
-    // Replace React with Preact only in client side for production builds
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
-    return config
-  },
-}
+module.exports = {}
