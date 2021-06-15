92406e545622d86f0c1f9ebaed4c87a32f2f09d6
Mario Beltrán Alarcón <belco90@gmail.com>
2021-06-15 22:20:09 +0200

    configure ESLint within nextjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,7 +27,7 @@ const App = ({ Component, pageProps }) => {
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
-  }, [router])
+  }, [router.events])
 
   return (
     <ChakraProvider theme={theme}>
