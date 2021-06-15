c1e74ec4c65a18bb77d1fee2f03c519f4e48cb31
Mario Beltrán Alarcón <belco90@gmail.com>
2021-06-16 00:12:45 +0200

    switch to nextjs web font optimization

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,15 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700"
+          />
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
