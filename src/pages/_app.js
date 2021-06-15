da839bdec70a2c4f503590c87cbc67e4dc19af9e
Mario Beltrán Alarcón <belco90@gmail.com>
2021-06-15 21:59:30 +0200

    update useEffect deps

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,7 +27,7 @@ const App = ({ Component, pageProps }) => {
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
-  }, [router.events])
+  }, [router])
 
   return (
     <ChakraProvider theme={theme}>
