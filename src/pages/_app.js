d0ee92d834b31513d29da531a6cb808844a6941b
Mario Beltrán Alarcón <belco90@gmail.com>
2021-11-12 19:44:13 +0000

    remove gtag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,27 +1,13 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import { ChakraProvider } from '@chakra-ui/react'
 import { DefaultSeo } from 'next-seo'
 
 import MainLayout from '~/components/MainLayout'
 import SEO from '~/next-seo.config'
 import 'focus-visible/dist/focus-visible'
-import * as gtag from '~/lib/gtag'
 import theme from '~/theme'
 
 const App = ({ Component, pageProps }) => {
-  const router = useRouter()
   const Layout = Component.layout || MainLayout
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
 
   return (
     <ChakraProvider theme={theme}>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,8 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript } from '@chakra-ui/react'
 
-import { GA_TRACKING_ID } from '~/lib/gtag'
-
 class MyDocument extends Document {
   render() {
     return (
@@ -15,24 +13,6 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap"
-          />
-
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
           />
         </Head>
         <body>
