f4a807b21e23aac368b01b8b576c546c325e3257
Mario Beltrán Alarcón <belco90@gmail.com>
2022-01-19 20:27:50 +0100

    SEO improvements

--- src/default-seo.config.ts
+++ src/default-seo.config.ts
@@ -1,24 +1,25 @@
 import type { DefaultSeoProps } from 'next-seo'
 import { UserConfig } from '~/user.config'
 
-const description = `${UserConfig.author.name} - ${UserConfig.author.position}`
+const description = UserConfig.author.position
 
 const DefaultSeoConfig: DefaultSeoProps = {
   titleTemplate: `%s | ${UserConfig.author.name}`,
+  defaultTitle: UserConfig.author.name,
   description,
   openGraph: {
     type: 'website',
     url: 'https://mario.dev',
-    title: description,
+    title: UserConfig.author.name,
     images: [
       {
         url: 'https://en.gravatar.com/userimage/2579648/589fd5525e1be579480a610887aa79df.png?size=200',
         width: 200,
         height: 200,
-        alt: "Mario's profile picture",
+        alt: 'Thoughtful Mario',
       },
     ],
-    site_name: `${UserConfig.author.name}'s website`,
+    site_name: description,
   },
   twitter: {
     handle: '@belcoDev',
