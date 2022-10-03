ee46cde11b54b44d2d89b059e4cff8d6784d3d86
Mario Beltrán Alarcón <belco90@gmail.com>
2022-10-03 18:35:50 +0200

    Remove ignored files from lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,25 @@
 const path = require('node:path')
+const { ESLint } = require('eslint')
 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
+const removeIgnoredFiles = async (files) => {
+	const eslint = new ESLint()
+	const ignoredFiles = await Promise.all(
+		files.map((file) => eslint.isPathIgnored(file))
+	)
+	return files.filter((_, i) => !ignoredFiles[i])
+}
+
+const buildEslintCommand = async (filenames) => {
+	const filteredFilenames = await removeIgnoredFiles(filenames)
+	const filenamesFlag = filteredFilenames
+		.map((filename) => path.relative(process.cwd(), filename))
+		.map((filename) => `--file ${filename}`)
+		.join(' ')
+
+	return `next lint --fix ${filenamesFlag}`
+}
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  '*': 'prettier --write --ignore-unknown',
+	'*.{js,jsx,ts,tsx}': [buildEslintCommand],
+	'*': 'prettier --write --ignore-unknown',
 }
