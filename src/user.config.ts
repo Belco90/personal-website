98ae1c13c3f52ea79af9bb6526e5198ae165da0c
Mario Beltrán Alarcón <belco90@gmail.com>
2022-10-10 14:29:40 +0000

    Fix mailto link

--- src/user.config.ts
+++ src/user.config.ts
@@ -4,7 +4,7 @@ const UserConfig = {
 		position: 'Frontend Software Engineer',
 	},
 	social: {
-		email: 'mariobeltran@onmail.com',
+		email: 'mailto:mariobeltran@onmail.com',
 		twitter: 'https://twitter.com/belcoDev',
 		github: 'https://github.com/Belco90',
 		linkedin: 'https://www.linkedin.com/in/mario-ba-90/?locale=en_US',
