9eed5450a7d9804103ce9d767a7f1400e7b21ad1
Mario Beltrán Alarcón <belco90@gmail.com>
2022-10-31 14:49:35 +0000

    Remove GoatCounter analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,16 +7,12 @@ import { DefaultSeo } from 'next-seo'
 import MainLayout from '~/components/MainLayout'
 import { DefaultSeoConfig } from '~/default-seo.config'
 import { theme } from '~/theme'
-import { GoatCounterScript, useGoatCounter } from '~/goat-counter-utils'
 
 type AppProps = NextAppProps
 
 const App = ({ Component, pageProps }: AppProps) => {
-	useGoatCounter()
-
 	return (
 		<>
-			<GoatCounterScript />
 			<Analytics />
 			<ChakraProvider theme={theme}>
 				<DefaultSeo {...DefaultSeoConfig} />
