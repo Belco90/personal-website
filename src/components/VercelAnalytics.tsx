201d3b10183e0617bc83f0e9f7f4a8c5f4078f4e
Mario Beltrán <belco90@gmail.com>
2022-12-09 16:10:53 +0100

    Simplify Vercel Analytics (#47)
    * Bump Next.js ecosystem dependencies
    
    * Bump Vercel analytics
    
    * Simplify Vercel analytics logic

--- src/components/VercelAnalytics.tsx
+++ src/components/VercelAnalytics.tsx
@@ -1,27 +1,32 @@
 import { Analytics } from '@vercel/analytics/react'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 const VA_DISABLE_KEY = 'va-disable'
 const VA_TOGGLE_HASH = '#va-toggle'
+
+function getIsVercelAnalyticsDisabled(): boolean {
+	const currentUrl = new URL(document.URL)
+	return (
+		Boolean(currentUrl.searchParams.get(VA_DISABLE_KEY)) ||
+		Boolean(localStorage.getItem(VA_DISABLE_KEY))
+	)
+}
 
 function logToggleVercelAnalyticsAction(action: 'disabled' | 'enabled') {
 	// eslint-disable-next-line no-console
 	console.log(`Vercel Analytics has been ${action} for this browser`)
 }
 
-const NoopVercelAnalytics = (): null => {
-	useEffect(() => {
-		// eslint-disable-next-line no-console
-		console.warn(
-			'Vercel Analytics is set up, but detected a non-Vercel production env. Please note that no analytics events will be sent.'
-		)
-	}, [])
-	return null
-}
-
-const ProductionVercelAnalytics = () => {
+function useToggleVercelAnalytics(): { isEnabled: boolean } {
 	const { isReady, replace, asPath } = useRouter()
+	const isEnabledRef = useRef<boolean>(false)
+
+	useEffect(() => {
+		const isDisabled = getIsVercelAnalyticsDisabled()
+		const statusText = isDisabled ? 'disabled' : 'enabled'
+		console.log(`Vercel Analytics are ${statusText} for this browser`)
+	}, [])
 
 	useEffect(() => {
 		const url = new URL(document.URL)
@@ -30,6 +35,7 @@ const ProductionVercelAnalytics = () => {
 		if (isReady && shouldToggleVercelAnalytics) {
 			const newUrl = asPath.replace(VA_TOGGLE_HASH, '')
 			const isVercelAnalyticsDisabled = !!localStorage.getItem(VA_DISABLE_KEY)
+			isEnabledRef.current = !isVercelAnalyticsDisabled
 
 			if (isVercelAnalyticsDisabled) {
 				localStorage.removeItem(VA_DISABLE_KEY)
@@ -42,31 +48,28 @@ const ProductionVercelAnalytics = () => {
 		}
 	}, [replace, isReady, asPath])
 
+	return { isEnabled: isEnabledRef.current }
+}
+
+const VercelAnalytics = () => {
+	const { isEnabled } = useToggleVercelAnalytics()
+
+	const isVercelProductionEnv =
+		process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+
 	return (
 		<Analytics
 			beforeSend={(event) => {
-				const url = new URL(event.url)
-
-				if (url.searchParams.get(VA_DISABLE_KEY)) {
-					return null
-				}
-
-				if (localStorage.getItem(VA_DISABLE_KEY)) {
+				if (!isEnabled) {
 					return null
 				}
 
 				return event
 			}}
+			mode={isVercelProductionEnv ? 'production' : 'development'}
+			debug={!isVercelProductionEnv}
 		/>
 	)
-}
-
-const VercelAnalytics = () => {
-	if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
-		return <NoopVercelAnalytics />
-	}
-
-	return <ProductionVercelAnalytics />
 }
 
 export default VercelAnalytics
