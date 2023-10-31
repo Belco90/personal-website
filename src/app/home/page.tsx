3ae60264ba861bdf9562ed4b46d23b44e6c79e35
Mario Beltrán <belco90@gmail.com>
2023-10-31 16:50:28 +0100

    Add first iteration of the Header

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,11 @@
 import { type FC } from 'react'
 
 import { panda } from '@/styled-system/jsx'
-import ThemeSwitchIconButton from '~/components/ThemeSwitchIconButton'
 
 const HomePage: FC = () => {
 	return (
 		<div>
-			<panda.h1 color="emerald.500">Hello 🐼!</panda.h1>
-			<ThemeSwitchIconButton />
+			<panda.h1 color="primary.500">Hello 🐼!</panda.h1>
 		</div>
 	)
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+import { type FC } from 'react'
+
+import {
+	panda,
+	Container,
+	Flex,
+	Box,
+	Spacer,
+	HStack,
+} from '@/styled-system/jsx'
+import ThemeSwitchIconButton from '~/components/ThemeSwitchIconButton'
+
+const Header: FC = () => {
+	return (
+		<panda.header
+			zIndex="banner"
+			shadow="lg"
+			width="full"
+			bgColor={{ base: 'white', _dark: 'gray.700' }}
+		>
+			<Container py={2} maxWidth="breakpoint-md">
+				<Flex width="full" align="center">
+					<Box
+						fontWeight="bold"
+						fontSize="2xl"
+						fontFamily="heading"
+						bgGradient="to-r"
+						gradientFrom={{ base: 'primary.50', _dark: 'primary.500' }}
+						gradientTo={{ base: 'primary.500', _dark: 'primary.700' }}
+						bgSize="100% 0.2em"
+						bgPosition="0 80%"
+						bgRepeat="no-repeat"
+					>
+						Mario
+					</Box>
+
+					<Spacer />
+
+					<HStack gap={{ base: 2, lg: 8 }} alignItems="center">
+						<panda.nav
+							display="flex"
+							gap={[2, 4]}
+							alignItems="center"
+							justifyContent="center"
+						>
+							<a href="/">Home</a>
+							<a href="/projects">Projects</a>
+						</panda.nav>
+						<ThemeSwitchIconButton />
+					</HStack>
+				</Flex>
+			</Container>
+		</panda.header>
+	)
+}
+
+export default Header
