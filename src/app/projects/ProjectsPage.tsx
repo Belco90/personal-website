f315d526c0d486e5118692c31cf584f9836b1b27
Mario Beltrán Alarcón <belco90@gmail.com>
2023-11-01 13:01:15 +0100

    Move projects page

--- src/app/projects/ProjectsPage.tsx
+++ src/app/projects/ProjectsPage.tsx
@@ -1,5 +1,4 @@
-import { Container, Heading, VStack } from '@chakra-ui/react'
-
+import { Container, panda, VStack } from '@/styled-system/jsx'
 import ProjectCard from '~/components/ProjectCard'
 import type { Project } from '~/models'
 
@@ -9,11 +8,11 @@ interface ProjectsPageProps {
 
 const ProjectsPage = ({ projects }: ProjectsPageProps) => {
 	return (
-		<Container maxWidth="container.md">
-			<Heading as="h1" variant="gradient" mb={4} fontSize="4xl">
+		<Container maxWidth="breakpoint-md">
+			<panda.h1 mb="4" fontSize="4xl" textGradient="heading">
 				Projects
-			</Heading>
-			<VStack spacing={6}>
+			</panda.h1>
+			<VStack gap="6">
 				{projects.map(({ repo, npmPackage }) => (
 					<ProjectCard key={repo.id} repo={repo} npmPackage={npmPackage} />
 				))}
--- theme/semantic-tokens.ts
+++ theme/semantic-tokens.ts
@@ -10,8 +10,23 @@ export const semanticTokens = defineSemanticTokens({
 			value:
 				'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;',
 		},
+		card: {
+			value: {
+				base: '0.2rem 0.2em {colors.primary.100}',
+				_dark: '0.2rem 0.2em {colors.primary.600}',
+			},
+		},
 	},
 	durations: {
 		'social-network': { value: '0.3s' },
+	},
+	gradients: {
+		heading: {
+			value: {
+				base: 'linear-gradient(to bottom right, {colors.primary.400}, {colors.primary.700})',
+				_dark:
+					'linear-gradient(to bottom right, {colors.primary.200}, {colors.primary.400})',
+			},
+		},
 	},
 })
