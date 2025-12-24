bafd60ae13b119c8513110db9d86520d5d6f119e
Mario Beltrán <me@mario.dev>
2025-12-24 08:59:32 +0100

    Update typescript-eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -8,13 +8,10 @@ export default defineConfig(
 	eslint.configs.recommended,
 	tseslint.configs.recommendedTypeChecked,
 	tseslint.configs.stylisticTypeChecked,
-	// TODO: enable equivalent to eslint-config-next
 	{
 		languageOptions: {
-			parser: tseslint.parser,
 			parserOptions: {
 				projectService: true,
-				tsconfigRootDir: import.meta.dirname,
 			},
 		},
 	},
@@ -32,21 +29,12 @@ export default defineConfig(
 					default: 'generic',
 				},
 			],
-			'@typescript-eslint/no-floating-promises': 'error',
-			'@typescript-eslint/consistent-type-exports': [
-				'error',
-				{
-					fixMixedExportsWithInlineTypeSpecifier: true,
-				},
-			],
+			'@typescript-eslint/consistent-type-exports': ['error'],
+			'@typescript-eslint/consistent-type-imports': ['error'],
 
-			'@typescript-eslint/consistent-type-imports': [
-				'error',
-				{
-					fixStyle: 'inline-type-imports',
-				},
-			],
-			'@typescript-eslint/no-explicit-any': 'error',
+			// Disabling because of index errors on interfaces, which works fine in type aliases:
+			// https://bobbyhadz.com/blog/typescript-index-signature-for-type-is-missing-in-type
+			'@typescript-eslint/consistent-type-definitions': 'off',
 
 			// Import
 			// 'import/newline-after-import': 'error',
@@ -83,11 +71,16 @@ export default defineConfig(
 			// 'react/self-closing-comp': 'warn',
 		},
 	},
-	// Config files
+	// Plain JS files
 	{
 		files: ['**/*.js', '**/*.mjs', '**/*.cjs'],
 		extends: [tseslint.configs.disableTypeChecked],
 	},
 	prettierRecommended,
-	globalIgnores(['**/node_modules', '**/public', '**/styled-system']),
+	globalIgnores([
+		'**/node_modules',
+		'**/public',
+		'**/styled-system',
+		'**/dist',
+	]),
 )
