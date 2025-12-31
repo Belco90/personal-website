2dce81124a3065fa433bf634753301672e970166
Mario Beltrán <me@mario.dev>
2025-12-31 10:50:03 +0100

    Suppress hydration warning
    Only caused by the theme mode since can't be fully derived
    in the server.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -57,7 +57,7 @@ export const Route = createRootRoute({
 
 function RootLayout() {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<head>
 				<HeadContent />
 			</head>
